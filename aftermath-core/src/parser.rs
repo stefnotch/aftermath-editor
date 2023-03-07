f582abd27fb0821b8f8717508fd34cf67711ecb4
stefnotch <brandmairstefan@gmail.com>
2023-03-07 18:45:40 +0100

    Refactor brackets parsing

--- aftermath-core/src/parser.rs
+++ aftermath-core/src/parser.rs
@@ -61,6 +61,11 @@ impl<'a> MathSemanticArgsContinuation {
     }
 }
 
+pub struct ParseResult<T> {
+    pub value: T,
+    pub errors: Vec<ParseError>,
+}
+
 #[derive(Debug, Clone)]
 pub struct ParseError {
     pub error: ParseErrorType,
@@ -138,7 +143,6 @@ fn parse_bp(lexer: &mut Lexer, context: &ParseContext, minimum_bp: u32) -> MathS
             None => break,
             Some(v) => match v {
                 MathElement::Symbol(s) => s,
-                MathElement::Bracket(s) => s,
                 v => panic!("unexpected element {:?}", v),
             },
         };
@@ -203,6 +207,7 @@ fn parse_bp(lexer: &mut Lexer, context: &ParseContext, minimum_bp: u32) -> MathS
 fn parse_bp_start<'a, 'b>(
     token: Option<&'a MathElement>,
     context: &'b ParseContext,
+    // TODO: Use ParseResult here, so that you can report multiple errors, and always can return a value
 ) -> Result<MathSemanticArgsSubRowsContinuation<'a>, ParseError> {
     match token {
         Some(v) => match v {
@@ -235,6 +240,28 @@ fn parse_bp_start<'a, 'b>(
                         minimum_bp: definition.binding_power.1.unwrap(),
                         sub_rows: vec![],
                     })
+                } else if let Some(definition) =
+                    context.get_bracket_definition(s, BracketType::Opening)
+                {
+                    // Bracket opening
+                    // TODO: quotes are like brackets
+
+                    Ok(MathSemanticArgsSubRowsContinuation {
+                        // This gives me one slightly redundant layer of nesting for brackets, but it's not a big deal
+                        math_semantic: MathSemantic {
+                            name: "bracket".to_string(),
+                            args: vec![],
+                            value: definition.name.clone().into_bytes(),
+                            range: (0, 0),
+                        },
+                        parse_args: |a, b, c| {
+                            let left = vec![parse_bp(a, b, c)];
+                            a.next(); // TODO: Do this instead: assert_eq!(a.next(), Some(&definition.closing_bracket));
+                            left
+                        },
+                        minimum_bp: 0,
+                        sub_rows: vec![],
+                    })
                 } else {
                     // Undefined symbol
                     // TODO: What if that symbol is a postfix operator or an infix operator?
@@ -263,16 +290,6 @@ fn parse_bp_start<'a, 'b>(
                 minimum_bp: 0,
                 sub_rows: vec![top, bottom],
             }),
-            /*
-            MathElement::Bracket(_s) => {
-                // TODO: check if opening bracket
-                // quotes are like brackets hmm
-                let left = parse_bp(lexer, context, 0);
-                // TODO: Check for the correct closing bracket
-                assert_eq!(lexer.next(), Some(&MathElement::Bracket(")".to_string())));
-                Ok(left)
-
-            } */
             MathElement::Sup(_) | MathElement::Sub(_) => Err(ParseError {
                 error: ParseErrorType::UnexpectedPostfixOperator,
                 range: (0, 0),
@@ -289,14 +306,19 @@ fn parse_bp_start<'a, 'b>(
 pub struct ParseContext<'a> {
     // takes ownership of the parent context and gives it back afterwards
     parent_context: Option<&'a ParseContext<'a>>,
+    // TODO: Use a Hash Array Mapped Trie here (see issue #20)
     known_tokens: HashMap<TokenKey, TokenDefinition>,
-    // TODO: brackets
+    known_brackets: HashMap<BracketKey, BracketDefinition>,
     // functions
     // ...
 }
 
 impl<'a> ParseContext<'a> {
-    pub fn new(tokens: Vec<(String, TokenDefinition)>) -> ParseContext<'a> {
+    pub fn new(
+        // TODO: Compute the string from the token definition
+        tokens: Vec<(String, TokenDefinition)>,
+        brackets: Vec<(String, BracketDefinition)>,
+    ) -> ParseContext<'a> {
         ParseContext {
             parent_context: None,
             known_tokens: tokens
@@ -312,6 +334,28 @@ impl<'a> ParseContext<'a> {
                         },
                         v,
                     )
+                })
+                .collect(),
+            known_brackets: brackets
+                .into_iter()
+                .flat_map(|(k, v)| {
+                    vec![
+                        (
+                            BracketKey {
+                                pattern: k.clone(),
+                                bracket_type: BracketType::Opening,
+                            },
+                            // TODO: This is a copy
+                            v.clone(),
+                        ),
+                        (
+                            BracketKey {
+                                pattern: k,
+                                bracket_type: BracketType::Closing,
+                            },
+                            v,
+                        ),
+                    ]
                 })
                 .collect(),
         }
@@ -333,48 +377,67 @@ impl<'a> ParseContext<'a> {
                     .and_then(|v| v.get_token_definition(operator, bp_pattern))
             })
     }
+
+    fn get_bracket_definition(
+        &self,
+        bracket: &str,
+        bracket_type: BracketType,
+    ) -> Option<&BracketDefinition> {
+        self.known_brackets
+            .get(&BracketKey {
+                // This does a copy, but it's fine
+                pattern: bracket.to_string(),
+                bracket_type,
+            })
+            .or_else(|| {
+                self.parent_context
+                    .and_then(|v| v.get_bracket_definition(bracket, bracket_type))
+            })
+    }
 }
 
 impl<'a> ParseContext<'a> {
     pub fn default() -> ParseContext<'a> {
-        ParseContext::new(vec![
-            (
-                "+".to_string(),
-                TokenDefinition::new("Add".to_string(), (Some(100), Some(101))),
-            ),
-            (
-                "-".to_string(),
-                TokenDefinition::new("Subtract".to_string(), (Some(100), Some(101))),
-            ),
-            (
-                "+".to_string(),
-                TokenDefinition::new("Add".to_string(), (None, Some(400))),
-            ),
-            (
-                "-".to_string(),
-                TokenDefinition::new("Subtract".to_string(), (None, Some(400))),
-            ),
-            (
-                "*".to_string(),
-                TokenDefinition::new("Multiply".to_string(), (Some(200), Some(201))),
-            ),
-            (
-                "/".to_string(),
-                TokenDefinition::new("Divide".to_string(), (Some(200), Some(201))),
-            ),
-            (
-                ".".to_string(),
-                TokenDefinition::new("Ring".to_string(), (Some(501), Some(500))),
-            ),
-            (
-                "!".to_string(),
-                TokenDefinition::new("Ring".to_string(), (Some(600), None)),
-            ),
-            /*(
-                ")".to_string(),
-                TokenDefinition::new("Ring".to_string(), (Some(600), None)),
-            ),*/
-        ])
+        ParseContext::new(
+            vec![
+                (
+                    "+".to_string(),
+                    TokenDefinition::new("Add".to_string(), (Some(100), Some(101))),
+                ),
+                (
+                    "-".to_string(),
+                    TokenDefinition::new("Subtract".to_string(), (Some(100), Some(101))),
+                ),
+                (
+                    "+".to_string(),
+                    TokenDefinition::new("Add".to_string(), (None, Some(400))),
+                ),
+                (
+                    "-".to_string(),
+                    TokenDefinition::new("Subtract".to_string(), (None, Some(400))),
+                ),
+                (
+                    "*".to_string(),
+                    TokenDefinition::new("Multiply".to_string(), (Some(200), Some(201))),
+                ),
+                (
+                    "/".to_string(),
+                    TokenDefinition::new("Divide".to_string(), (Some(200), Some(201))),
+                ),
+                (
+                    ".".to_string(),
+                    TokenDefinition::new("Ring".to_string(), (Some(501), Some(500))),
+                ),
+                (
+                    "!".to_string(),
+                    TokenDefinition::new("Ring".to_string(), (Some(600), None)),
+                ),
+            ],
+            vec![(
+                "(".to_string(),
+                BracketDefinition::new("()".to_string(), "(".to_string(), ")".to_string()),
+            )],
+        )
     }
 }
 
@@ -408,6 +471,40 @@ impl TokenDefinition {
         TokenDefinition {
             name,
             binding_power,
+        }
+    }
+}
+
+#[derive(Hash, Eq, PartialEq)]
+struct BracketKey {
+    pattern: String,
+    bracket_type: BracketType,
+}
+
+#[derive(Hash, Eq, PartialEq, Clone, Copy)]
+enum BracketType {
+    Opening,
+    Closing,
+}
+
+#[derive(Clone)]
+pub struct BracketDefinition {
+    name: String,
+    // TODO: Have a parser or something for this
+    opening_bracket: MathElement,
+    closing_bracket: MathElement,
+}
+
+impl BracketDefinition {
+    pub fn new(
+        name: String,
+        opening_bracket: String,
+        closing_bracket: String,
+    ) -> BracketDefinition {
+        BracketDefinition {
+            name,
+            opening_bracket: MathElement::Symbol(opening_bracket),
+            closing_bracket: MathElement::Symbol(closing_bracket),
         }
     }
 }
