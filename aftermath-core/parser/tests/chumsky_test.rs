31155aa685e3b2d994a3a849c97fc397bcaba407
stefnotch <brandmairstefan@gmail.com>
2023-08-19 13:50:11 +0200

    Update chumsky

--- aftermath-core/parser/tests/chumsky_test.rs
+++ aftermath-core/parser/tests/chumsky_test.rs
@@ -14,40 +14,14 @@ pub enum SyntaxTreeInstruction {
 
 fn parser<'a>(/* TODO: Add a "instruction transformer" */
 ) -> impl Parser<'a, &'a [InputNode], SyntaxTreeInstruction> {
-    let mut atom = just(InputNode::Symbol("cat".into()))
-        .map(|node| SyntaxTreeInstruction::StartChildren)
-        .boxed()
-        .or(just(InputNode::Symbol("dog".into()))
-            .map(|node| SyntaxTreeInstruction::StartChildren)
-            .boxed())
-        .boxed();
-
-    for i in 0..100 {
-        atom = atom
-            .or(just(InputNode::Symbol(format!("cat{}", i).into()))
-                .map(|node| SyntaxTreeInstruction::StartChildren)
-                .boxed())
-            .boxed();
-    }
-
-    /* let atom = choice((
+    let atom = choice(vec![
         just(InputNode::Symbol("cat".into()))
             .map(|node| SyntaxTreeInstruction::StartChildren)
             .boxed(),
         just(InputNode::Symbol("dog".into()))
             .map(|node| SyntaxTreeInstruction::StartChildren)
             .boxed(),
-    )); */
-
-    // Doesn't work, due to https://github.com/zesterer/chumsky/issues/484
-    /*let atom = choice(vec![
-        just(InputNode::Symbol("cat".into()))
-            .map(|node| SyntaxTreeInstruction::StartChildren)
-            .boxed(),
-        just(InputNode::Symbol("dog".into()))
-            .map(|node| SyntaxTreeInstruction::StartChildren)
-            .boxed(),
-    ]);*/
+    ]);
 
     let operator = choice((
         left_infix(just(InputNode::Symbol("+".into())), 1, |l, r| {
