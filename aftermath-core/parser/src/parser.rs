1d20b4540707b8e33110fa5cccde869aaee84a9a
stefnotch <brandmairstefan@gmail.com>
2023-08-28 14:05:05 +0200

    Chumsky might work

--- aftermath-core/parser/src/parser.rs
+++ aftermath-core/parser/src/parser.rs
@@ -7,7 +7,7 @@ use crate::{
     autocomplete::{AutocompleteMatcher, AutocompleteRule},
     greedy_choice::greedy_choice,
     rule_collection::{BindingPowerType, InputPhantom, RuleCollection, TokenRule},
-    syntax_tree::SyntaxNode,
+    syntax_tree::{SyntaxNode, SyntaxNodeChildren},
     BoxedTokenParser, TokenParser, TokenParserExtra,
 };
 
@@ -23,6 +23,37 @@ struct CachedMathParser {
     token_rules: Arc<Vec<TokenRule>>,
 }
 
+fn combine_ranges(a: std::ops::Range<usize>, b: std::ops::Range<usize>) -> std::ops::Range<usize> {
+    let start = a.start.min(b.start);
+    let end = a.end.max(b.end);
+    start..end
+}
+
+fn build_prefix_syntax_node(op: SyntaxNode, rhs: SyntaxNode) -> SyntaxNode {
+    SyntaxNode::new(
+        op.name.clone(),
+        combine_ranges(op.range(), rhs.range()),
+        SyntaxNodeChildren::Children(vec![op, rhs]),
+    )
+}
+
+fn build_postfix_syntax_node(op: SyntaxNode, lhs: SyntaxNode) -> SyntaxNode {
+    SyntaxNode::new(
+        op.name.clone(),
+        combine_ranges(op.range(), lhs.range()),
+        SyntaxNodeChildren::Children(vec![lhs, op]),
+    )
+}
+
+fn build_infix_syntax_node(op: SyntaxNode, children: [SyntaxNode; 2]) -> SyntaxNode {
+    let [lhs, rhs] = children;
+    SyntaxNode::new(
+        op.name.clone(),
+        combine_ranges(op.range(), combine_ranges(lhs.range(), rhs.range())),
+        SyntaxNodeChildren::Children(vec![lhs, op, rhs]),
+    )
+}
+
 /// See https://github.com/zesterer/chumsky/blob/f10e56b7eac878cbad98f71fd5485a21d44db226/src/lib.rs#L3456
 impl Cached for CachedMathParser {
     type Parser<'src> = BoxedTokenParser<'src, 'src>;
@@ -30,6 +61,8 @@ impl Cached for CachedMathParser {
     fn make_parser<'src>(self) -> Self::Parser<'src> {
         let mut token_parsers = vec![];
         let mut prefix_parsers = vec![];
+        let mut postfix_parsers = vec![];
+        let mut infix_parsers = vec![];
 
         for rule in self.token_rules.iter() {
             let rule_parser = (rule.make_parser)(&rule, InputPhantom::new());
@@ -39,42 +72,46 @@ impl Cached for CachedMathParser {
                     token_parsers.push(rule_parser);
                 }
                 BindingPowerType::Prefix(strength) => {
-                    prefix_parsers.push(chumsky::pratt::prefix(rule_parser, strength, |rhs| {
-                        SyntaxNode::new(todo!(), todo!(), todo!())
-                    }));
+                    prefix_parsers.push(chumsky::pratt::prefix(
+                        rule_parser,
+                        strength,
+                        build_prefix_syntax_node,
+                    ));
                 }
-                BindingPowerType::Postfix(strength) => todo!(),
-                BindingPowerType::LeftInfix(strength) => todo!(),
-                BindingPowerType::RightInfix(strength) => todo!(),
+                BindingPowerType::Postfix(strength) => {
+                    postfix_parsers.push(chumsky::pratt::postfix(
+                        rule_parser,
+                        strength,
+                        build_postfix_syntax_node,
+                    ));
+                }
+                BindingPowerType::LeftInfix(strength) => {
+                    infix_parsers.push(chumsky::pratt::left_infix(
+                        rule_parser,
+                        strength,
+                        build_infix_syntax_node,
+                    ));
+                }
+                BindingPowerType::RightInfix(strength) => {
+                    infix_parsers.push(chumsky::pratt::right_infix(
+                        rule_parser,
+                        strength,
+                        build_infix_syntax_node,
+                    ));
+                }
             }
         }
 
-        /*   let operator = greedy_choice(vec![
-            chumsky::pratt::left_infix(
-                chumsky::primitive::just(InputNode::Symbol("+".into())),
-                1,
-                |l, r| SyntaxNode::new(todo!(), todo!(), todo!()),
-            ),
-            chumsky::pratt::left_infix(
-                chumsky::primitive::just(InputNode::Symbol("-".into())),
-                1,
-                |l, r| SyntaxNode::new(todo!(), todo!(), todo!()),
-            ),
-        ]);*/
-
-        let operator = chumsky::pratt::left_infix(
-            chumsky::primitive::choice(vec![
-                chumsky::primitive::just(InputNode::Symbol("+".into())),
-                chumsky::primitive::just(InputNode::Symbol("-".into())),
-            ]),
-            1,
-            |l, r| SyntaxNode::new(todo!(), todo!(), todo!()),
-        );
-
+        // Here's to hoping that greedy_choice doesn't devolve into exponential time
         let atom = greedy_choice(token_parsers);
-        let prefix = chumsky::primitive::choice(prefix_parsers);
+        let operator = greedy_choice(infix_parsers);
+        let prefix = greedy_choice(prefix_parsers);
+        let postfix = greedy_choice(postfix_parsers);
 
-        let expr = atom.pratt(operator).with_prefix_ops(prefix);
+        let expr = atom
+            .pratt(operator)
+            .with_prefix_ops(prefix)
+            .with_postfix_ops(postfix);
 
         expr.boxed()
     }
